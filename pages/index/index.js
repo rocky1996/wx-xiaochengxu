adfe9be43fb51001c39aeeeb459fa0244ce4477a
rocky1996 <2791752775@qq.com>
2018-11-08 13:30:08 +0800

    添加远程访问接口实例

--- pages/index/index.js
+++ pages/index/index.js
@@ -12,10 +12,57 @@ Page({
     indicatorDots: false,
     autoplay: false,
     interval: 5000,
-    duration: 1000
+    duration: 1000,
+    proList: null,
   },
   
   onLoad: function () {
-    
+    this.setData({
+      test:'01',
+    })
+    this.getProList();
+  },
+  toDetail: function (e){
+    console.log(e);
+    var index = e.currentTarget.dataset.index;
+    console.log(index);
+
+    var proList = this.data.proList;
+    var title = proList[index].title;
+    wx.navigateTo({
+      url: '/pages/detail/detail?title=' + title,
+    })
+  },
+  getProList: function(){
+    var self = this;
+    wx.request({
+      url: '',
+      method: 'GET',
+      success: function(res){
+        console.log(res);
+        self.setData({
+          proList: res.data,
+        })
+      }
+    })
+  },
+
+  copy: function(){
+    if(wx.setClipboardData){
+      wx.setClipboardData({
+        data: '这是我要复制的内容',
+        success: function (res) {
+          wx.showModal({
+            title: '复制成功',
+            content: '内容已经复制成功',
+          })
+        }
+      })
+    }else{
+      wx.showModal({
+        title: '提示',
+        content: '您的微信版本太低，情升级',
+      })
+    } 
   }
 })
